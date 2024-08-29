0f2f7d54e49a5c8baf61724424879dadc071e021
gareth0712 <gareth_0712@hotmail.com>
2024-08-29 23:13:02 +0900

    refractor scripts in anchor-cli

--- buffalo_joe/rust_solana/anchor-cli/scripts/hello-solana.ts
+++ buffalo_joe/rust_solana/anchor-cli/scripts/hello-solana.ts
@@ -2,44 +2,15 @@ import {
   Keypair,
   Connection,
   PublicKey,
-  LAMPORTS_PER_SOL,
   TransactionInstruction,
   Transaction,
   sendAndConfirmTransaction,
 } from '@solana/web3.js';
 import fs from 'mz/fs';
-import path from 'path';
 
 import 'dotenv/config';
 
-/*
-  Keypair we used to create the on-chain Rust program
-*/
-const PROGRAM_KEYPAIR_PATH = path.join(
-  path.resolve(__dirname, '../dist/program'),
-  'hello_solana-keypair.json'
-);
-
-const loadKeypairFromFile = async (filePath: string): Promise<Keypair> => {
-  const absolutePath = path.resolve(filePath);
-  const keypairData = JSON.parse(fs.readFileSync(absolutePath, 'utf-8'));
-  return Keypair.fromSecretKey(Uint8Array.from(keypairData));
-}
-
-const generateKeypair = async (connection): Promise<Keypair> => {
-  /*
-  Generate an account (keypair) to transact with our program
-  */
-  const triggerKeypair = Keypair.generate();
-
-  const airdropRequest = await connection.requestAirdrop(
-    triggerKeypair.publicKey,
-    LAMPORTS_PER_SOL,
-  );
-  await connection.confirmTransaction(airdropRequest);
-
-  return triggerKeypair;
-}
+import { generateKeypair, getProgramKeypairPath, loadKeypairFromFile } from '@/utils';
 
 async function main() {
   // run the following command in cli ahead to ensure you can get the log message properly: 
@@ -54,18 +25,23 @@ async function main() {
   /*
   Get our program's public key (ensure you have deployed the program on devnet)
   */
-  const secretKeyString = await fs.readFile(PROGRAM_KEYPAIR_PATH, {encoding: 'utf8'});
+  const programKeypairPath: string = getProgramKeypairPath('hello_solana-keypair.json');
+  const secretKeyString = await fs.readFile(programKeypairPath, {encoding: 'utf8'});
   const secretKey = Uint8Array.from(JSON.parse(secretKeyString));
-  const programKeypair = Keypair.fromSecretKey(secretKey);
+  const programKeypair: Keypair = Keypair.fromSecretKey(secretKey);
   let programId: PublicKey = programKeypair.publicKey;
   console.log('Program ID is: ', programId)
 
   /*
     Keypair of our solana account (ensure there is +ve SOL balance)
   */
-  const triggerKeypair = await loadKeypairFromFile(process.env.KEYPAIR_ID_PATH);
-  // Use this function if you don't have an existing keypair
-  // const triggerKeypair = await generateKeypair(connection);
+  let triggerKeypair;
+  const keypairPath: string | undefined = process.env.KEYPAIR_ID_PATH;
+  if (keypairPath) {
+    triggerKeypair = await loadKeypairFromFile(keypairPath);
+  } else {
+    triggerKeypair = await generateKeypair(connection);
+  }
   console.log('Account public key is: ', triggerKeypair.publicKey);
 
   /*
@@ -97,7 +73,7 @@ async function main() {
 main().then(
   () => process.exit(),
   err => {
-    console.log('Error occurred runnin the cli program')
+    console.log('Error occurred running the cli program')
     console.error(err);
     process.exit(-1);
   },
