24193b2684741d624536fc39270dd6165e700d08
gareth0712 <gareth_0712@hotmail.com>
2024-09-02 22:02:49 +0900

    update readme and formatting for learning file

--- career_booster_beginner/19_networking/src/main.rs
+++ career_booster_beginner/19_networking/src/main.rs
@@ -32,5 +32,19 @@ fn useClient() -> Result<()> {
 
 // 2) UDP
 // If you want to use the User Datagram Protocol, which provides no guarantees about the order in which frames are sent nor guarantees delivery, you can use the 👉 UdpSocket.
+fn createUDPServer() -> std::io::Result<()> {
+  let socket = UdpSocket::bind("127.0.0.1:34254")?;
+
+  // Receives a single datagram message on the socket. If `buf` is too small to hold
+  // the message, it will be cut off.
+  let mut buf = [0; 10];
+  let (amt, src) = socket.recv_from(&mut buf)?;
+
+  // Redeclare `buf` as slice of the received data and send reverse data back to origin.
+  let buf = &mut buf[..amt];
+  buf.reverse();
+  socket.send_to(buf, &src)?;
+  Ok(())
+}
 
 fn main() -> Result<()> {}
