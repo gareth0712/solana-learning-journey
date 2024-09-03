901cfaa2062ca4e44492119117ae3889e06035a2
gareth0712 <gareth_0712@hotmail.com>
2024-09-03 17:21:30 +0900

    add learning files (to be continued) for sequences, maps and sets; textual types; smart pointeres

---
+++ career_booster_beginner/14_sequences_maps_sets/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+  // A VecDeque<T> is optimized for adding and removing elements from either side of the sequence. It is most commonly used to realize a first-in-first-out queue.
+  // A LinkedList<T> implements a doubly linked list where the nodes contain two references and a value T in line. Linked lists are interesting from a theoretical perspective, but they are not often used in practice as they have a significant memory overhead and are not very cache efficient.
+  // TODO: Continue on other parts
+}
